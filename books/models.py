e36bc60ca78dfc8945e714b52983c751c9b4a62f
Yuriy Golota <ypgolota@pm.me>
2022-07-20 12:23:41 -0400

    built out dir & file tree

--- books/models.py
+++ books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.constraints import UniqueConstraint
 
-# Create your models here.
+class User(AbstractUser):
+    def __str__(self):
+        return self.username
+
+    def __repr__(self):
+        return f"<User username={self.username} pk={self.pk}>"
